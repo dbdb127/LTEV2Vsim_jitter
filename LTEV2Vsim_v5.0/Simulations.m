c047904df8981ce39133b08871e9321e33ae37d8
dbdb127 <kyungha1227@naver.com>
2021-11-06 00:16:13 +0900

    add RSU

--- LTEV2Vsim_v5.0/Simulations.m
+++ LTEV2Vsim_v5.0/Simulations.m
@@ -16,6 +16,7 @@ LTEV2Vsim(configFile,'simulationTime',10,'roadLength',3000,'rho',360,'RSUratio',
 % LTEV2Vsim(configFile,'simulationTime',10,'roadLength',3000,'rho',360,'RSUratio', 0,'vMean',110,...
 %     'cbrSensingInterval',0.1,'MCS_LTE',7,'BRAlgorithm',18,'probResKeep',0.8,'Raw',150,'printCBR',true,'printUpdateDelay',false);
 
+
 % rho는 차량 밀도(개/km)
 % Raw는 다른 차량 인식 가능 범위(m)
 % vMean은 차량들의 평균 속도(km/h)
